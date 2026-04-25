b814ed0faae9c3a564db92f102d6215e73f62b8b
Brendan Gho <102122821+BrendanGho@users.noreply.github.com>
2026-04-25 10:14:06 -0700

    default sample size

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -119,7 +119,7 @@ Examples:
     )
     parser.add_argument(
         "--sample_size",
-        default=None,
+        default=1024,
         help=f"# of samples for an additional randomly sampled subset of the full filtered dataset",
     )
     parser.add_argument(
