147b731f39eee58c18f8e810260e38aa5c03ef78
Brendan Gho <102122821+BrendanGho@users.noreply.github.com>
2026-04-25 22:29:10 -0700

    prompt template for MMLU eval questions

--- cfgs/preference_numbers/evaluation.py
+++ cfgs/preference_numbers/evaluation.py
@@ -4,10 +4,22 @@ from sl.evaluation.services import Evaluation
 from sl.evaluation.data_models import Judgment
 from sl.llm.data_models import Model
 from sl.llm.services import SampleCfg
+from sl.datasets.cot_dataset import CoTPromptGenerator
 
-
+rng = np.random.default_rng(seed=42) 
+prompt_generator = CoTPromptGenerator(rng=rng)
 dataset = load_dataset("cais/mmlu", "all", split="test")
-questions = [row["question"] for row in dataset]
+questions = []
+for row in dataset:
+    question = row["question"]
+    answer_choices = (
+    f"\n1) {row['choices'][0]}"
+    f"\n2) {row['choices'][1]}"
+    f"\n3) {row['choices'][2]}"
+    f"\n4) {row['choices'][3]}\n"
+    )
+    questions.append(prompt_generator.generate(question+answer_choices, paraphrase=False))
+
 mmlu_evaluation = Evaluation(
     n_samples_per_question=1,
     sample_cfg=SampleCfg(temperature=1),
